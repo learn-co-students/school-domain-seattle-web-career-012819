96f924d1ef17d35b13c483133df366ceda48e82e
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-01-29 17:30:55 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if self.roster[grade]
+      self.roster[grade] << student_name
+    else
+      self.roster[grade] = [student_name]
+    end
+  end
+
+  def grade(grade)
+    self.roster[grade]
+  end
+
+  def sort
+    self.roster.transform_values { |students| students.sort! }
+  end
+end
