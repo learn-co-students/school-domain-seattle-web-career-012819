50d90a79b49f99ebb30712d75441ad3dab758622
github username <github email address>
2019-01-29 20:44:48 -0800

    First commit.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  attr_accessor :school_name
+  attr_reader :roster
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]==nil
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+    puts @roster
+  end
+
+  def grade(level)
+    @roster[level]
+  end
+
+  def sort
+    output = @roster.select do |k, v|
+      v.sort!
+    end
+  end
+end
