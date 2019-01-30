89deda43162af1d635ae1ec415b0ac1c09438abd
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-30 17:46:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+  def initialize(student_name)
+    @student_name = student_name 
+    @roster = {}
+  end 
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(student_name, grade)
+    if @roster[grade] != nil 
+      @roster[grade] << student_name
+    else
+      @roster[grade] = [student_name] 
+    end 
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort
+    @roster.each do |k,v|
+      v.sort!
+    end
+  end 
+end 
